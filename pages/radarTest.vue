e4372df3d089b63d64fbaec5ed80e8418712f7cc
Bilal Khan <bilal.khan@bjss.com>
2024-01-04 16:11:18 +0000

    Added radar container component, which can take a list of blips and draw them

--- pages/radarTest.vue
+++ pages/radarTest.vue
@@ -1,17 +1,28 @@
 <template>
-	<svg width="400" height="400">
-		<RadarBackground />
-		<RadarBlip x="80" y="70" color="lightblue" text="Something" />
-		<RadarBlip x="100" y="100" color="lightgreen" text="Something else" />
-		<RadarBlip x="250" y="70" color="lavender" text="Vue?" />
-		<RadarBlip x="50" y="300" color="aquamarine" text="Nuxt?" />
-	</svg>
+	<Radar size="400", sectors="5" :blips="blips" />
 </template>
 
 <script setup>
-	provide('width', 400);
-	provide('height', 400);
-	provide('sectors', 4);
+	let blips = [
+	{
+	x: '80',
+	y: '70',
+	color: 'lightblue',
+	text: 'Something'
+	},
+	{
+	x: '100',
+	y: '100',
+	color: 'lightblue',
+	text: 'Something'
+	},
+	{
+	x: '250',
+	y: '70',
+	color: 'lightblue',
+	text: 'Something'
+	},
+	];
 </script>
 
 <style>
